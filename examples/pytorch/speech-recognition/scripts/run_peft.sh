30d33b5a52135a72396143d6f917bbf7947aaae7
seas2nada <dooh1474@naver.com>
2024-03-24 14:48:57 +0000

    initial commit

---
+++ examples/pytorch/speech-recognition/scripts/run_peft.sh
@@ -0,0 +1,36 @@
+torchrun \
+ 	--nproc_per_node $GPUS run_speech_recognition_seq2seq_peft.py \
+	--deepspeed="deepspeed_configs/ds_config_zero2.json" \
+	--model_name_or_path="openai/whisper-large-v3" \
+	--dataset_name=$data \
+	--dataset_config_name=$data_config \
+	--train_split_name=$train_subset \
+	--eval_split_name=$eval_subset \
+	--max_steps="30000" \
+	--output_dir="./whisper-large-KlecSpeech-peft-max30000-test" \
+	--per_device_train_batch_size="12" \
+	--per_device_eval_batch_size="12" \
+	--logging_steps="25" \
+	--learning_rate="1e-3" \
+	--warmup_steps="500" \
+	--evaluation_strategy="steps" \
+	--eval_steps="10" \
+	--save_strategy="steps" \
+	--save_steps="1000" \
+	--generation_max_length="225" \
+	--length_column_name="input_length" \
+	--max_duration_in_seconds="30" \
+	--ddp_timeout "86400" \
+	--freeze_feature_encoder "False" \
+	--audio_column_name=$audio_column_name \
+	--text_column_name=$text_column_name \
+	--preprocessing_num_workers=$num_proc \
+	--language="korean" \
+	--preprocessing_only="False" \
+	--group_by_length \
+	--fp16 \
+	--overwrite_output_dir \
+	--do_train \
+	--do_eval \
+	--predict_with_generate \
+	--use_auth_token
--- tools/install_modules.sh
+++ tools/install_modules.sh
@@ -1,4 +1,5 @@
 . ./activate_python.sh
 pip install --upgrade git+https://github.com/huggingface/transformers.git
-pip install datasets[audio] evaluate jiwer
+pip install datasets[audio] evaluate jiwer bitsandbytes
 pip install accelerate -U
+pip install -q git+https://github.com/huggingface/transformers.git@main git+https://github.com/huggingface/peft.git@main
